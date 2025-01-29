26aa68c36141a870646593f13efeb229d105c37d
ManojParmar93 <manojparmar5606@gmail.com>
2025-01-29 13:15:46 +0530

    added spec to handle delimeters

--- spec/services/string_calculator_spec.rb
+++ spec/services/string_calculator_spec.rb
@@ -5,5 +5,26 @@ RSpec.describe StringCalculator do
     it 'should respond to add method' do
       expect(StringCalculator.respond_to?(:add)).to be_truthy
     end
+
+    it 'returns 0 for an empty string' do
+      expect(StringCalculator.add("")).to eq(0)
+    end
+
+    it 'returns the number itself for a single number' do
+      expect(StringCalculator.add("1")).to eq(1)
+    end
+
+    it 'returns the sum of two numbers' do
+      expect(StringCalculator.add("1,5")).to eq(6)
+    end
+
+    it 'handles new lines between numbers' do
+      expect(StringCalculator.add("1\n2,3")).to eq(6)
+    end
+
+    it 'supports different delimiters' do
+      expect(StringCalculator.add("//;\n1;2")).to eq(3)
+      expect(StringCalculator.add("//|\n1|2|3")).to eq(6)
+    end
   end
 end
