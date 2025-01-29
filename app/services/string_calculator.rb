6b7f3125c59357a5b92a4f1b2a16ce1e71a8d5bc
ManojParmar93 <manojparmar5606@gmail.com>
2025-01-29 13:17:38 +0530

    added logic for string number addition and handle delimiters

--- app/services/string_calculator.rb
+++ app/services/string_calculator.rb
@@ -1,4 +1,10 @@
 class StringCalculator
+  DELIMITERS = [' ', "//", "\n", ";", "|"]
+
   def self.add(numbers)
+    # replaced all delimiters with comma
+    DELIMITERS.each { |delimiter| numbers.gsub!(delimiter, ',') }
+
+    numbers.split(',').collect(&:to_i).sum
   end
 end
